af12f195dac39f245f391fe990ab4541d08eeec3
Andre Fonseca <aoqfonseca@gmail.com>
2015-10-01 16:43:14 -0300

    Removing comments and empty lines from settings file.

--- schedule_twitter/settings.py
+++ schedule_twitter/settings.py
@@ -1,48 +1,19 @@
-"""
-Django settings for schedule_twitter project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$oeprvl$xdx#4**m7srd*#w1qgxt0l9n7yg44pn=x!fbu6t_bl'
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'suit',
@@ -66,12 +37,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'schedule_twitter.urls'
-
 WSGI_APPLICATION = 'schedule_twitter.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,27 +51,16 @@ LOCAL_DATABASE_PATH = 'sqlite:////%s' % os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES['default'] = dj_database_url.config(default=LOCAL_DATABASE_PATH)
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'pt-BR'
 TIME_ZONE = 'America/Sao_Paulo'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-#Twitter
 APP_KEY = os.environ.get('APP_TWITTER_ID')
 APP_SECRET = os.environ.get('APP_TWITTER_SECRET')
 APP_URL = os.environ.get('APP_URL', 'http://localhost:8000')
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
